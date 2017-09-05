20594e76d81f7af624e69f411d2cc63dd9e3dc3b
Curtis Rueden <ctrueden@wisc.edu>
2017-09-05 11:07:46 -0500

    Travis: fix javadoc destination

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-javadoc.sh
-sh travis-javadoc.sh ImageJ $encrypted_0bd0ae40f1e4_key $encrypted_0bd0ae40f1e4_iv
+sh travis-javadoc.sh Fiji $encrypted_0bd0ae40f1e4_key $encrypted_0bd0ae40f1e4_iv
